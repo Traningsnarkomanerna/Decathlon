4afbfacf951a8a0b44aa535e23c151d5a1238470
daniellavrban <daniellavrban@hotmail.com>
2023-09-18 13:17:32 +0200

    Added test for Deca400M. Created a new method to access the score.

---
+++ src/test/java/testrunner/Deca400MTest.java
@@ -0,0 +1,24 @@
+package testrunner;
+
+import decathlon.Deca400M;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Deca400MTest {
+    @Test
+    public void shouldCalculate400m() {
+        Deca400M deca400M = new Deca400M();
+        double A = 1.53775;
+        double B = 82;
+        double C = 1.81;
+
+        double runningTime = 50.0;
+        int expectedScore = (int) (A * Math.pow((B - runningTime), C));
+        deca400M.calculateResult(runningTime);
+
+        int actualScore = deca400M.getScore();
+
+        assertEquals(expectedScore, actualScore);
+    }
+}
